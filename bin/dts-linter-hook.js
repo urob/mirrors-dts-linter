f9ee9f9a7ce9fbad990d929896f4aef84ea896ef
urob <978080+urob@users.noreply.github.com>
2026-05-18 13:33:59 -0400

    Resolve hook path dynamically

--- bin/dts-linter-hook.js
+++ bin/dts-linter-hook.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 const { spawnSync } = require('child_process');
+const { join, dirname } = require('path');
+
+const pkg = require('dts-linter/package.json');
+const binRelPath = typeof pkg.bin === 'string' ? pkg.bin : pkg.bin['dts-linter'];
+const dtsLinter = join(dirname(require.resolve('dts-linter/package.json')), binRelPath);
 
 const argv = process.argv.slice(2);
 const flags = argv.filter(a => a.startsWith('-'));
@@ -9,8 +14,12 @@ const files = argv.filter(a => !a.startsWith('-'));
 if (!files.length) process.exit(0);
 
 const result = spawnSync(
-  'dts-linter',
+  dtsLinter,
   [...flags, ...files.flatMap(f => ['--file', f])],
   { stdio: ['inherit', 'ignore', 'inherit'] },
 );
+if (result.error) {
+  process.stderr.write(`dts-linter-hook: ${result.error.message}\n`);
+  process.exit(1);
+}
 process.exit(result.status ?? 1);
